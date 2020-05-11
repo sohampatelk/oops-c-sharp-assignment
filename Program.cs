8a86cf74abd3d92e5d5815c09961f4ef14e9df82
sohampatelk <sohampatelk@yahoo.com>
2020-05-11 16:25:00 -0600

    concepts done

--- Program.cs
+++ Program.cs
@@ -23,16 +23,60 @@ namespace oop_c_sharp_assignment {
             set { medium = value; }
         }
 
-        public string MyYear{
+        public int MyYear{
             get { return year; }
             set { year = value; }
         }
 
+        public virtual void sameMethodInBothClass(){
+            System.Console.WriteLine("This is method of same name method...but in Painting class");
+        } 
+
+        public void methodOfPainting(){
+            System.Console.WriteLine("Method Of Painting...");
+        }
+
+    }
+
+    class Painter:Painting{
+        public override void sameMethodInBothClass(){
+            System.Console.WriteLine("This is a method of same name method...but in Painter Class");
+        }
+
+        public void methodPainter(){
+            System.Console.WriteLine("Method Painter");
+        }
     }
     class Program {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Encapsulation
+            Painting myPainting = new Painting();
+            myPainting.MyName = "abcde";
+            myPainting.MyArtist = "xyz";
+            myPainting.MyMedium = "def";
+            myPainting.MyYear = 2020;
+
+            //Inheritance
+            Painter newPaint = new Painter();
+
+            System.Console.WriteLine("Name : "+myPainting.MyName);
+            System.Console.WriteLine("Artist : "+myPainting.MyArtist);
+            System.Console.WriteLine("Medium : "+myPainting.MyMedium);
+            System.Console.WriteLine("Year : "+myPainting.MyYear);
+
+
+            // Polymorphism
+            Painting p1 = new Painting();
+            Painting p2 = new Painter();
+            p1.sameMethodInBothClass();
+            p2.sameMethodInBothClass();
+
+            // Abstraction
+            p2.methodOfPainting();
+            
         }
     }
 }
